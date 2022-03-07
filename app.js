1c75c009ff8d4a93c933d79495940d59c5a090a5
John Rock <johnmrock.jr@gmail.com>
2022-03-07 10:09:26 -0500

    Updates

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const apiURL = 'https://api.color.pizza/v1/';
+const apiURL = "https://api.color.pizza/v1/";
 
 // Get complementary color from hex value
 function getComplimentaryColor(hex) {
   // strip # from hex value
-  hex = hex.replace('#', '');
+  hex = hex.replace("#", "");
 
   // convert hex value to RGB
   r = parseInt(hex.substring(0, 2), 16);
@@ -22,17 +22,17 @@ function getComplimentaryColor(hex) {
 
   // pad with 0s
   if (r.length < 2) {
-    r = '0' + r;
+    r = "0" + r;
   }
   if (g.length < 2) {
-    g = '0' + g;
+    g = "0" + g;
   }
   if (b.length < 2) {
-    b = '0' + b;
+    b = "0" + b;
   }
 
   // return HEX value
-  return '#' + r + g + b;
+  return "#" + r + g + b;
 }
 
 async function getColors() {
@@ -50,25 +50,49 @@ async function getColors() {
   const colorComplement = getComplimentaryColor(colorHex);
   const colorName = color.name;
 
+  // Generate an array of complimentary
+  const colors = chroma
+    .scale([colorHex, colorComplement])
+    .mode("lch")
+    .colors(6);
+
+  // Remove first color form array becuase it is the same as the original color
+  colors.shift();
+
+  // Build html for the comp colors
+  const compColors = colors
+    .map((color) => {
+      return `
+      <div class="color-box" style="background-color: ${color}"></div>
+    `;
+    })
+    .join("");
+
+  // Insert complementary color boxes into DOM
+  document
+    .getElementById("color-comp")
+    .insertAdjacentHTML("afterbegin", compColors);
+
   // Check to see if the background is too light to read text
+  // Adjust text so its readable
+  // TODO: Improve this logic
   if (color.luminance >= 130) {
-    console.log('yes');
-    document.body.style.filter = 'invert(70%)';
+    document.body.style.filter = "invert(70%)";
   } else {
     document.body.style.color = colorComplement;
   }
 
   const body = document.body;
-  const colorNameElement = document.querySelector('.color-name');
-  const colorHexElement = document.querySelector('.color-hex');
-  const colorRgbElement = document.querySelector('.color-rgb');
+  const colorNameElement = document.querySelector(".color-name");
+  const colorHexElement = document.querySelector(".color-hex");
+  const colorRgbElement = document.querySelector(".color-rgb");
 
   body.style.backgroundColor = colorHex;
   body.style.color = colorComplement;
   colorNameElement.textContent = colorName;
   colorHexElement.textContent = colorHex;
   colorRgbElement.textContent = `rgb(${colorR}, ${colorG}, ${colorB})`;
-  document.querySelector('footer').style.opacity = 1;
+  document.querySelector("footer").style.opacity = 1;
 }
 
 getColors();
